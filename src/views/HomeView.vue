4ceae2d559f6dab59d60a553a69f2201e6774da9
Thaenkhun <tankoon300950@gmail.com>
2024-01-25 16:02:41 +0700

    first app

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="card" style="width: 18rem;">
+  <img src="https://positioningmag.com/wp-content/uploads/2018/01/4_esport.png">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
